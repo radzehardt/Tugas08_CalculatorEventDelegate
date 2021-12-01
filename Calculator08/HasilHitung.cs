663a29888e2b4bacffaa68fcabeb4b7eda670d92
radzehardt <95371882+radzehardt@users.noreply.github.com>
2021-12-01 23:59:16 +0700

    Tambah Tugas

---
+++ Calculator08/HasilHitung.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator08
+{
+    public partial class HasilHitung : Form
+    {
+        private List<Kalkulator> listDariHasil = new List<Kalkulator>();
+        public HasilHitung()
+        {
+            InitializeComponent();
+            initialisasilv();
+        }
+
+        private void initialisasilv()
+        {
+            lvHasil.View = View.Details;
+
+            lvHasil.Columns.Add("", 5, HorizontalAlignment.Left);
+            lvHasil.Columns.Add("", 192, HorizontalAlignment.Left);
+
+
+        }
+        private void FrmKalkulator_OnCalculated(Kalkulator kalkulasi)
+        {
+            listDariHasil.Add(kalkulasi);
+
+            ListViewItem daftar = new ListViewItem();
+            daftar.SubItems.Add(kalkulasi.HasilPerhitungan);
+
+            lvHasil.Items.Add(daftar);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Perhitungan perhitungan = new Perhitungan("Calculator");
+            perhitungan.OnCalculated += FrmKalkulator_OnCalculated;
+            perhitungan.ShowDialog();
+        }
+    }
+}
---
+++ Calculator08/Perhitungan.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator08
+{
+    public partial class Perhitungan : Form
+    {
+        public delegate void KalkulasiEventHandler(Kalkulator kalkulasi);
+        public event KalkulasiEventHandler OnCalculated;
+
+        private Kalkulator kalkulasi;
+        public Perhitungan()
+        {
+            InitializeComponent();
+        }
+
+        public Perhitungan(string judul) : this()
+        {
+            this.Text = judul;
+        }
+
+        private void btnProses_Click(object sender, EventArgs e)
+        {
+            kalkulasi = new Kalkulator();
+            int a = int.Parse(txtNilaiA.Text);
+            int b = int.Parse(txtNilaiB.Text);
+
+            switch (cmbOP.Text)
+            {
+                case "Penjumlahan":
+                    kalkulasi.HasilPerhitungan = string.Format("Hasil Penjumlahan {0} + {1} = {2}", a, b, a + b);
+                    break;
+                case "Pengurangan":
+                    kalkulasi.HasilPerhitungan = string.Format("Hasil Pengurangan {0} - {1} = {2}", a, b, a - b);
+                    break;
+                case "Perkalian":
+                    kalkulasi.HasilPerhitungan = string.Format("Hasil Perkalian {0} x {1} = {2}", a, b, a * b);
+                    break;
+                case "Pembagian":
+                    kalkulasi.HasilPerhitungan = string.Format("Hasil Pembagian {0} / {1} = {2}", a, b, a / b);
+                    break;
+                default:
+                    break;
+            }
+
+
+
+            OnCalculated(kalkulasi);
+            txtNilaiA.Clear();
+            txtNilaiB.Clear();
+            txtNilaiA.Focus();
+        }
+
+
+    }
+}
